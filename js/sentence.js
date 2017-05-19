60bc478251a640e2e93cfb447a457d217648876e
Breno Freitas <brenofreitasmain@gmail.com>
2017-05-19 15:54:01 -0300

    Enviando dados para o Éden 15:53

--- js/sentence.js
+++ js/sentence.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 	Typed.new(".element", {
-        strings: ["Eu sei que a sua auto estima está baixa  ", "Eu estava observando você o tempo inteiro.  ","Não se assuste  ","Porque eu tenho uma instrução para você","E ela vai lhe dar o propósito que a sua vida precisa  ","Eu sei o que você quer ","Porque os meus olhos estão por toda parte "
-,"Sim, você não tem para onde esconder, portanto... ", "Bem-vindo ao ÉDEN "],
-        typeSpeed: 160
+        strings: ["Olá!", "Preste bem atenção no que eu vou dizer...","Eu sei que a sua auto estima está baixa...", "Eu estava observando você o tempo inteiro.","Peço para que não se assuste","Porque eu tenho uma instrução para você...","E ela vai lhe dar o propósito que a sua vida precisa...","Eu sei o que você quer!","Porque os meus olhos estão por toda parte..."
+,"Sim, você não tem para onde esconder, portanto... ", "Bem-vindo ao ÉDEN"],
+        typeSpeed: 110
 	});
 });
